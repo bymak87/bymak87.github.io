8a5a91e68f40687fb029bc5526029ee1dc73ec40
Amy Smith <bymak87@gmail.com>
2021-02-03 17:08:09 -0600

    update settings

---
+++ gh-pages-site/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteTitle: 'Amelia Smith Resume', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/bymak87.github.io/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Amelia',
+  lastName: 'Smith',
+  heroSubheader: 'I enjoy solving business problems with design. Some might call me a digital Jill-of-all-trades. I can design, code, provide analytics, and help businesses with their online presence.',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/bymak87',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/abakameyer/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/bymatm',
+    },
+    {
+      icon: 'far fa-id-card',
+      name: 'Website',
+      url: 'https://www.bymadesigns.com/',
+    },
+  ],
+  email: 'bymak87@gmail.com',
+  phone: 'email for cell',
+  address: 'Allen, TX',
+};
